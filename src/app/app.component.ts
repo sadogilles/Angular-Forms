aaf7b98f7d3a5e322fcef280e0291a5eaf89811c
pablo <sadowizzy@gmail.com>
2022-06-05 09:41:57 +0200

    Forms Project raw initial version

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title= 'tdf';
+
+  topics = ['Angular','React', 'Vue'];
+
+  userModel =  {name:'',email: 'rob@email.com',phone: 99888999,topic:"Angular",timePreference:'morning',subscribe:true};
+
+}
---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+
+     name:string;
+     email:string;
+     phone:number;
+     topic:string;
+     timePreference:string;
+     subscribe:boolean;
+}
